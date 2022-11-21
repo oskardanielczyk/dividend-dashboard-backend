46cee49baf56411a1bb98c34be008ca65b132bb0
oskardanielczyk <oskar.danielczyk@gmail.com>
2022-11-21 19:30:27 +0100

    install cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const transactionRoutes = require("./routes/transactions.route");
 const userRoutes = require("./routes/users.route");
@@ -10,16 +11,15 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use(cors());
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
-  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
 
   next();
 });
